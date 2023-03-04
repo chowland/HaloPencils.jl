fbe2f119277df19c5492c67584ed056b1f17612b
chowland <c.j.howland@utwente.nl>
2023-03-04 14:30:10 +0100

    avoid confusing decomp_dims name

--- test/halo_rank_test.jl
+++ test/halo_rank_test.jl
@@ -5,9 +5,9 @@ using Test
 
 MPI.Init()
 dims = (8, 36, 36)
-decomp_dims = MPI.Dims_create(MPI.Comm_size(MPI.COMM_WORLD), (0,0))
+proc_dims = MPI.Dims_create(MPI.Comm_size(MPI.COMM_WORLD), (0,0))
 periodic = (true, false)
-comm_cart = MPI.Cart_create(MPI.COMM_WORLD, decomp_dims; periodic=periodic)
+comm_cart = MPI.Cart_create(MPI.COMM_WORLD, proc_dims; periodic=periodic)
 topo = MPITopology{2}(comm_cart)
 pen = Pencil(topo, dims)
 
@@ -25,22 +25,22 @@ lvy, lvz = lvlhalo
 @test all(A[:, 1+lvy:end-lvy, 1+lvz:end-lvz] .== rank)
 
 coords = MPI.Cart_coords(comm_cart)
-rank_b = mod(rank - decomp_dims[2], nranks)
-rank_t = mod(rank + decomp_dims[2], nranks)
+rank_b = mod(rank - proc_dims[2], nranks)
+rank_t = mod(rank + proc_dims[2], nranks)
 
 # # Test lower halo
 if coords[1]==1 && !periodic[1]
     rank_b = rank
 else
-    rank_b = mod(rank - decomp_dims[2], nranks)
+    rank_b = mod(rank - proc_dims[2], nranks)
 end
 @test all(A[:,1:lvy, 1+lvz:end-lvz] .== rank_b)
 
 # # Test upper halo
-if coords[1]+1==decomp_dims[1] && !periodic[1]
+if coords[1]+1==proc_dims[1] && !periodic[1]
     rank_t = rank
 else
-    rank_t = mod(rank + decomp_dims[2], nranks)
+    rank_t = mod(rank + proc_dims[2], nranks)
 end
 @test all(A[:,end-lvy+1:end, 1+lvz:end-lvz] .== rank_t)
 
