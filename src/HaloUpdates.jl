fee188331feb138a1b148131c1bd6bcf021d34fb
chowland <c.j.howland@utwente.nl>
2023-03-04 15:33:18 +0100

    avoid MPI sendrecv for local copy

--- src/HaloUpdates.jl
+++ src/HaloUpdates.jl
@@ -56,33 +56,44 @@ function update_halo!(A::PencilArray, lvlhalo::Dims{M}) where M
             tag_t = coords[i] + 1
         end
 
-        neigh_b, neigh_t = MPI.Cart_shift(comm, i-1,1)
-
         n_end = size(A)[n]
-        T = typeof(A[1,1,1])
-        
-        # Copy data from PencilArray to send buffers
-        send_halo_b = copy(selectdim(A, n, 1+lvlhalo[i]:2*lvlhalo[i]))
-        send_halo_t = copy(selectdim(A, n, n_end-2*lvlhalo[i]+1:n_end-lvlhalo[i]))
-        recv_halo_b = similar(send_halo_b)
-        recv_halo_t = similar(send_halo_t)
 
-        # Receive from bottom
-        rreqb = MPI.Irecv!(recv_halo_b, comm; source=neigh_b, tag=tag_b)
-        # Receive from top
-        rreqt = MPI.Irecv!(recv_halo_t, comm; source=neigh_t, tag=tag_t)
-        # Send to bottom
-        sreqb = MPI.Isend(send_halo_b, comm; dest=neigh_b, tag=tag_b)
-        # Send to top
-        sreqt = MPI.Isend(send_halo_t, comm; dest=neigh_t, tag=tag_t)
+        # If target process is local, don't use MPI sends
+        if tag_t == tag_b
+            if periodic[i]
+                selectdim(A, n, 1:lvlhalo[i]) .= selectdim(A, n, n_end-2*lvlhalo[i]+1:n_end-lvlhalo[i])
+                selectdim(A, n, n_end-lvlhalo[i]+1:n_end) .= selectdim(A, n, 1+lvlhalo[i]:2*lvlhalo[i])
+            end
+        else
 
-        MPI.Waitall([sreqb, sreqt, rreqb, rreqt])
-        
-        if periodic[i] || tag_b > 0
-            selectdim(A, n, 1:lvlhalo[i]) .= recv_halo_b
-        end
-        if tag_t < dims[i]
-            selectdim(A, n, n_end-lvlhalo[i]+1:n_end) .= recv_halo_t
+            neigh_b, neigh_t = MPI.Cart_shift(comm, i-1,1)
+
+            T = typeof(A[1,1,1])
+            
+            # Copy data from PencilArray to send buffers
+            send_halo_b = copy(selectdim(A, n, 1+lvlhalo[i]:2*lvlhalo[i]))
+            send_halo_t = copy(selectdim(A, n, n_end-2*lvlhalo[i]+1:n_end-lvlhalo[i]))
+            recv_halo_b = similar(send_halo_b)
+            recv_halo_t = similar(send_halo_t)
+
+            # Receive from bottom
+            rreqb = MPI.Irecv!(recv_halo_b, comm; source=neigh_b, tag=tag_b)
+            # Receive from top
+            rreqt = MPI.Irecv!(recv_halo_t, comm; source=neigh_t, tag=tag_t)
+            # Send to bottom
+            sreqb = MPI.Isend(send_halo_b, comm; dest=neigh_b, tag=tag_b)
+            # Send to top
+            sreqt = MPI.Isend(send_halo_t, comm; dest=neigh_t, tag=tag_t)
+
+            MPI.Waitall([sreqb, sreqt, rreqb, rreqt])
+            
+            if periodic[i] || tag_b > 0
+                selectdim(A, n, 1:lvlhalo[i]) .= recv_halo_b
+            end
+            if tag_t < dims[i]
+                selectdim(A, n, n_end-lvlhalo[i]+1:n_end) .= recv_halo_t
+            end
+
         end
     end
 end
